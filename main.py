e14a0de7bca628bdb2e9fa60a11959a7ff0e9f49
nemo <patryk13500@o2.pl>
2021-02-28 14:10:02 +0100

    P

---
+++ main.py
@@ -0,0 +1,100 @@
+# zad1
+# int=1
+# int2=2
+
+# float = 2.6
+# float2 = 2.7
+
+# string = "ala"
+# string2 = "ala ma kota"
+
+# z = complex('1+2j')
+# x = complex('1')
+# print(z)
+
+# print(int,int2)
+# print(float,float2)
+# print(string,string2)
+# print(z,x)
+
+#zad2
+# a = 1
+# b = 2
+# c = 3
+# dodawanie=a+b
+# print(dodawanie)
+# odejmowanie=a-b
+# print(odejmowanie)
+# mnożenie=a*b
+# print(mnożenie)
+# dzielenie=a/b
+# print(dzielenie)
+# dzielenie_cal=a//b
+# print(dzielenie_cal)
+# modulo=c%b
+# print(modulo)
+# potega=a**b
+# print(potega)
+
+#zad3
+# a = 1
+# b = 2
+# c = 3
+# d = 4
+# e = 5
+# f = 6
+
+# a+=1
+# print(a)
+
+# b-=1
+# print(b)
+
+# c*=2
+# print(c)
+
+# d/=2
+# print(d)
+
+# e**=2
+# print(e)
+
+# f%=2
+# print(f)
+
+#zad 4
+# from math import *
+# print(e**10)
+# print(log(5+sin(radians(8)**2)**(1/6)))
+
+#zad5
+# imie='PATRYK'
+# nazwisko='SZCZEPKOWSKI'
+# print(imie.capitalize(), nazwisko.capitalize())
+
+#zad6
+# slowa='la sta la pa la'
+# x = slowa.count('la')
+# print(x)
+
+#zad7
+# imie='Patryk'
+# print(imie[0],imie[5])
+
+#zad8
+# txt = "la sta la pa la"
+
+# x = txt.split()
+
+# print(x)
+
+#zad9
+
+# s = 'format'
+# f = 2.4
+# x = 0x1E8
+
+# print('string %(s)s' % {'s': s})
+# print('float %(f)f' % {'f': f})
+# print('szestnastkowe %(x)X.' % {'x': x})
+
